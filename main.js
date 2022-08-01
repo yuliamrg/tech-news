496aec0d3a1eeb904219f943097da1a850dcf49c
yuliamrg <yuliam.rivera@outlook.com>
2022-08-01 01:38:35 -0500

    primer commit

---
+++ main.js
@@ -0,0 +1,6 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {document.querySelector('.nav-menu').classList.toggle('show');});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', {delay: 150});
+ScrollReveal().reveal('.cards-baner-one', {delay: 500});
+ScrollReveal().reveal('.cards-baner-two', {delay: 500});
